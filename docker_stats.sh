073843172f0cfbe7cffb59d57ee54eccce384cd5
Wei Ma <wei.ma@sage.com>
2020-08-23 20:42:41 -0700

    sort works correctly now

--- docker_stats.sh
+++ docker_stats.sh
@@ -1,9 +1,20 @@
 #!/usr/local/bin/bash
 
 # Display docker stats sorted by memory usage
+#https://unix.stackexchange.com/questions/11856/sort-but-keep-header-line-at-the-top
+# print the header (the first line of input)
+# and then run the specified command on the body (the rest of the input)
+# use it in a pipeline, e.g. ps | body grep somepattern
+body() {
+    IFS= read -r header
+    printf '%s\n' "$header"
+    "$@"
+}
+
 while :
 do
   clear
-  docker stats --no-stream --format "table {{.Name}}\t{{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}" | sort -k 9 -n
+  docker stats --no-stream --format "table {{.MemPerc}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.Name}}" | \
+    body sort -k5 -nr
   sleep 60
 done
